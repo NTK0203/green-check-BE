c82705b9a85c20dd1c379b4c74c0caec76f1d8bc
NTK0203 <newthink0203@naver.com>
2025-08-22 02:29:16 +0900

    feat: cors 헤더 허용

---
+++ src/main/java/com/greencheck/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.greencheck.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*") //임시 전체 허용 처리
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
